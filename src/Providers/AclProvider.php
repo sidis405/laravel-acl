2b0e3d111089934d9a1e90f1f75a40e340ef800d
Sidrit Trandafili <forge405@gmail.com>
2015-09-11 19:41:32 +0200

    First commit

---
+++ src/Providers/AclProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Sid\Acl\Providers;
+
+use Sid\Acl\Models\Permission;
+use Illuminate\Contracts\Auth\Access\Gate as GateContract;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AclProvider extends ServiceProvider
+{
+    
+    /**
+     * Register any application authentication / authorization services.
+     *
+     * @param  \Illuminate\Contracts\Auth\Access\Gate  $gate
+     * @return void
+     */
+    public function boot(GateContract $gate)
+    {
+        // Dynamically register permissions with Laravel's Gate.
+        foreach ($this->getPermissions() as $permission) {
+            $gate->define($permission->name, function ($user) use ($permission) {
+                return $user->hasPermission($permission);
+            });
+        }
+    }
+
+    /**
+     * Fetch the collection of site permissions.
+     *
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    protected function getPermissions()
+    {
+        return Permission::with('roles')->get();
+    }
+}
